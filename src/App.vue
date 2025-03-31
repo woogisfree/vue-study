5f4d1ffaab73da12c2bcdaf746a549c61dadab80
woogie <woogisfree@gmail.com>
2025-03-31 15:19:23 +0900

    feat: 데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,65 @@
-<template></template>
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>60 만원</p>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      // Define any data properties here
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    };
+  },
   components: {},
 };
 </script>
 
-<style></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
