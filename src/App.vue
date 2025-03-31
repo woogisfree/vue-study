a04b5e9c643103d2537ceae3400818f362d0f603
woogie <woogisfree@gmail.com>
2025-03-31 20:35:02 +0900

    feat: 동적인 UI 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
   </div>
 
   <div>
-    <h4>{{ products[0] }}</h4>
+    <img src="./assets/room0.jpg" class="room-img" />
+    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
     <p>50 만원</p>
     <button @click="신고수[0]++">허위매물신고</button>
     <span> 신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img" />
     <h4>{{ products[1] }}</h4>
     <p>70 만원</p>
     <button @click="신고수[1]++">허위매물신고</button>
     <span> 신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img" />
     <h4>{{ products[2] }}</h4>
     <p>60 만원</p>
     <button @click="신고수[2]++">허위매물신고</button>
@@ -29,6 +40,7 @@ export default {
   data() {
     return {
       // Define any data properties here
+      모달창열렸니: false,
       신고수: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -45,6 +57,34 @@ export default {
 </script>
 
 <style scoped>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 header {
   line-height: 1.5;
 }
